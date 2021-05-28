47452bd00dda19a9a95a29ec0b134a626085f154
Domain Admin <Administrator@CORP.LOCAL>
2021-05-28 06:28:23 -0700

    temp fix for v0.5

--- automation/post-config.py
+++ automation/post-config.py
@@ -1,4 +1,7 @@
 import urllib3
 import sys
+import subprocess
 
-print('Nothing to do!')
+#temp fix for v0.5
+print('temp fix for v0.5')
+subprocess.run(["powershell", "-Command", '[microsoft.win32.registry]::SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services", "fSingleSessionPerUser", 1)'])
