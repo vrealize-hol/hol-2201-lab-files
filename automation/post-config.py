08bdf176496b3c269b93525d8f9233a8b3efbce6
Benoit Serratrice <bserratrice@vmware.com>
2021-05-31 11:22:04 +0800

    rollback fix for v0.5

--- automation/post-config.py
+++ automation/post-config.py
@@ -1,7 +1,4 @@
 import urllib3
 import sys
-import subprocess
 
-#temp fix for v0.5
-print('temp fix for v0.5')
-subprocess.run(["powershell", "-Command", '[microsoft.win32.registry]::SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows NT\Terminal Services", "fSingleSessionPerUser", 1)'])
+print('Nothing to do!')
