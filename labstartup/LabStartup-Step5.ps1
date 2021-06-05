517047728b6ad567ad3d3a6a689f33b8ee63df84
Benoit Serratrice <bserratrice@vmware.com>
2021-06-05 11:37:46 +0800

    update k8s startup script

--- labstartup/LabStartup-Step5.ps1
+++ labstartup/LabStartup-Step5.ps1
@@ -38,14 +38,14 @@ $VSPHERE_WITH_TANZU_USERNAME = 'administrator@corp.local'
 $ENV:KUBECTL_VSPHERE_PASSWORD = 'VMware1!'
 
 # Connect to Supervisor cluster
-kubectl vsphere login --vsphere-username $VSPHERE_WITH_TANZU_USERNAME --server=$VSPHERE_WITH_TANZU_CONTROL_PLANE_IP --tanzu-kubernetes-cluster-name $VSPHERE_WITH_TANZU_CLUSTER_NAME --tanzu-kubernetes-cluster-namespace $VSPHERE_WITH_TANZU_CLUSTER_NAMESPACE --insecure-skip-tls-verify
+kubectl vsphere login --vsphere-username $VSPHERE_WITH_TANZU_USERNAME --server=$VSPHERE_WITH_TANZU_CONTROL_PLANE_IP --tanzu-kubernetes-cluster-name $VSPHERE_WITH_TANZU_CLUSTER_NAME --tanzu-kubernetes-cluster-namespace $VSPHERE_WITH_TANZU_CLUSTER_NAMESPACE --insecure-skip-tls-verify | Out-Null
 kubectl config use-context $VSPHERE_WITH_TANZU_CONTROL_PLANE_IP
 
 # Wait until it is applied
 Do {
     kubectl apply -f $(Join-Path $LabStartupBaseFolder "build/vsphere/wcp/rainpole/tkc-dev-project.yaml")
     if ($LastExitCode -ne 0) {
-        kubectl vsphere login --vsphere-username $VSPHERE_WITH_TANZU_USERNAME --server=$VSPHERE_WITH_TANZU_CONTROL_PLANE_IP --tanzu-kubernetes-cluster-name $VSPHERE_WITH_TANZU_CLUSTER_NAME --tanzu-kubernetes-cluster-namespace $VSPHERE_WITH_TANZU_CLUSTER_NAMESPACE --insecure-skip-tls-verify
+        kubectl vsphere login --vsphere-username $VSPHERE_WITH_TANZU_USERNAME --server=$VSPHERE_WITH_TANZU_CONTROL_PLANE_IP --tanzu-kubernetes-cluster-name $VSPHERE_WITH_TANZU_CLUSTER_NAME --tanzu-kubernetes-cluster-namespace $VSPHERE_WITH_TANZU_CLUSTER_NAMESPACE --insecure-skip-tls-verify | Out-Null
         Continue
     }
     Start-Sleep -Seconds 20
@@ -58,14 +58,20 @@ Do {
     Start-Sleep -Seconds 20
     $tkc = kubectl get tkc/$VSPHERE_WITH_TANZU_CLUSTER_NAME -n $VSPHERE_WITH_TANZU_CLUSTER_NAMESPACE -o json | ConvertFrom-Json
     if ($LastExitCode -ne 0) {
-        kubectl vsphere login --vsphere-username $VSPHERE_WITH_TANZU_USERNAME --server=$VSPHERE_WITH_TANZU_CONTROL_PLANE_IP --tanzu-kubernetes-cluster-name $VSPHERE_WITH_TANZU_CLUSTER_NAME --tanzu-kubernetes-cluster-namespace $VSPHERE_WITH_TANZU_CLUSTER_NAMESPACE --insecure-skip-tls-verify
+        kubectl vsphere login --vsphere-username $VSPHERE_WITH_TANZU_USERNAME --server=$VSPHERE_WITH_TANZU_CONTROL_PLANE_IP --tanzu-kubernetes-cluster-name $VSPHERE_WITH_TANZU_CLUSTER_NAME --tanzu-kubernetes-cluster-namespace $VSPHERE_WITH_TANZU_CLUSTER_NAMESPACE --insecure-skip-tls-verify | Out-Null
         Continue
     }
-    $workernodes = $tkc.status.nodeStatus | Get-Member | ForEach-Object { If ($_.Name -like "*workers*") { @{$_.Name = $tkc.status.nodeStatus.($_.Name) } } }
-    $workernodes | Format-Table -HideTableHeaders -AutoSize | Out-String
+    if ($tkc) { $workernodes = $tkc.status.nodeStatus | Get-Member | ForEach-Object { If ($_.Name -like "*workers*") { @{$_.Name = $tkc.status.nodeStatus.($_.Name) } } } }
+    if ($workernodes) { $workernodes | Format-Table -HideTableHeaders -AutoSize | Out-String }
 } 
 While (-Not $workernodes.Values.Contains("ready") -or $workernodes.Values.Contains("notready"))
 
+# Clean system pod
+$allpods = kubectl get pods --all-namespaces -o json | ConvertFrom-Json
+$allpods.items | where-object { $_.status.phase -eq "Failed" -and $_.status.reason -eq "NodeAffinity" } | ForEach-Object {
+    kubectl delete pod/$($_.metadata.name) -n $($_.metadata.namespace)
+}
+
 # Deploy app
 kubectl config use-context $VSPHERE_WITH_TANZU_CLUSTER_NAME
-kubectl apply -f (Join-Path $LabStartupBaseFolder "labfiles/HOL-2201-08/Module 7/cadvisor.yml")
+kubectl apply -f (Join-Path $LabStartupBaseFolder "build/vsphere/wcp/rainpole/dev-project/cadvisor.yml")
