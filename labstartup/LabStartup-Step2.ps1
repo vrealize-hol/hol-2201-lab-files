b2e015a4e26c22e32f212e501b30f5757baa6500
Benoit Serratrice <bserratrice@vmware.com>
2021-06-08 20:29:03 +0800

    startup - clean up alarm in vCenter

--- labstartup/LabStartup-Step2.ps1
+++ labstartup/LabStartup-Step2.ps1
@@ -1,2 +1,13 @@
-Write-Output "Start vRA (/opt/scripts/deploy.sh)"
+Write-Output "Start vRealize Automation (/opt/scripts/deploy.sh)"
 echo y | plink root@vr-automation.corp.local -pw VMware1! -noagent "nohup /opt/scripts/deploy.sh > /tmp/labstartup-deploy.out 2> /tmp/labstartup-deploy.err < /dev/null &"
+
+Write-Output "Disable datastore storage usage alarm"
+Get-Datastore local_esx-01a | Get-AlarmDefinition -Name "Datastore usage on disk" | Set-AlarmDefinition -enabled:$false | Out-Null
+Get-Datastore local_esx-02a | Get-AlarmDefinition -Name "Datastore usage on disk" | Set-AlarmDefinition -enabled:$false | Out-Null
+Get-Datastore local_esx-03a | Get-AlarmDefinition -Name "Datastore usage on disk" | Set-AlarmDefinition -enabled:$false | Out-Null
+Get-Datastore local_esx-04a | Get-AlarmDefinition -Name "Datastore usage on disk" | Set-AlarmDefinition -enabled:$false | Out-Null
+Get-Datastore local_esx-05a | Get-AlarmDefinition -Name "Datastore usage on disk" | Set-AlarmDefinition -enabled:$false | Out-Null
+
+Write-Output "Disable Host memory usage alarm"
+Get-VMHost esx-01a.corp.local | Get-AlarmDefinition -Name "Host memory usage" | Set-AlarmDefinition -enabled:$false | Out-Null
+Get-VMHost esx-02a.corp.local | Get-AlarmDefinition -Name "Host memory usage" | Set-AlarmDefinition -enabled:$false | Out-Null
