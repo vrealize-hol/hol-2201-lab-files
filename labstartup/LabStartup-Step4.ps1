0a5dd0d36610b0f8aca4a8403791518aa9c4993b
Benoit Serratrice <bserratrice@vmware.com>
2021-06-08 23:31:14 +0800

    workaround - deploy vRA later

--- labstartup/LabStartup-Step4.ps1
+++ labstartup/LabStartup-Step4.ps1
@@ -6,3 +6,7 @@ Get-ChildItem -path ".\serverfiles" -Directory -Force | ForEach-Object {
     $cmd = "echo y | pscp -r -l root -pw VMware1! -scp -noagent .\serverfiles\$($_.Name)\* $serverName`:/"
     Invoke-Expression -Command $cmd
 }
+
+#Deploy VRA
+Write-Output "Start vRealize Automation (/opt/scripts/deploy.sh)"
+cmd /c echo y | plink root@vr-automation.corp.local -pw VMware1! -noagent "nohup /opt/scripts/deploy.sh > /tmp/labstartup-deploy.out 2> /tmp/labstartup-deploy.err < /dev/null &"
